10b1252ae9c448a031c52522bf026c9c8dd8ec56
Jonte <highfestiva@gmail.com>
2016-06-19 01:40:00 +0200

    Hacked together a working version.

--- crd.py
+++ crd.py
@@ -5,14 +5,22 @@ class crd:
 		return -self.y
 	def lng(self):
 		return self.x
+	def to_coord(self):
+		return crd(self.x,-self.y)
 	def __add__(self, v):
 		return crd(self.x+v.x, self.y+v.y)
 	def __sub__(self, v):
 		return crd(self.x-v.x, self.y-v.y)
-	def __mul__(self, s):
-		return crd(self.x*s, self.y*s)
-	def __div__(self, s):
-		return crd(self.x/s, self.y/s)
+	def __mul__(self, f):
+		return crd(self.x*f, self.y*f)
+	def __truediv__(self, d):
+		return crd(self.x/d, self.y/d)
+	def __str__(self):
+		return 'lat:%3.3f, lng:%3.3f' % (self.lat(),self.lng())
+	def to_coord_str(self):
+		return 'x:%3.3f, y:%3.3f' % (self.x,self.y)
 
 def latlng(lat, lng):
+	'''Use left-handed coordinate system, as that's what used for images.
+	   Positive latitude thus defined as negative Y.'''
 	return crd(lng,-lat)
--- img2coords.py
+++ img2coords.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-'''Generates some .json heatpoint files from the input image.'''
+'''Hard-coded hack to generate JSON population coordinate files from input image.
+   Could easily be refactored to handle a mer generic case.'''
 
 from glob import glob
 from os import remove
@@ -8,20 +9,23 @@ from os.path import exists
 from PIL import Image
 from crd import *
 
-afghanistan = latlng(32,64)
-img = Image.open('af_population.png')
-pixels = img.load()
-img_width,img_height = img.size
+
+map_adjust_scale = 0.46173564
+map_adjust_offset = latlng(1.9564,3.546)
 
 def map_adjust(p):
-	'Hard-coded retrofit of the approximate coord to actual coord.'
-	return (p-afghanistan)*0.2915228*0.997 + afghanistan + latlng(1.9464+0.009,3.607-0.052)
+	'''Hard-coded transformation of arbitrarily chosen center and scale(and
+	   let's not forget the center of the image) to real-world coordinates.'''
+	return (p-country_center)*map_adjust_scale + country_center + map_adjust_offset
 
-def clamp_range(size, start, end, step):
-	return range(max(min(size,start)-step,0), max(min(size,end)-step,0), step)
+def map_inverse_adjust(p):
+	return (p-map_adjust_offset-country_center) / map_adjust_scale + country_center
 
-def get_col(x, y, size):
-	if size <= 1:
+def clamp_range(maxend, start, end, step):
+	return range(max(min(maxend,start)-step,0), max(min(maxend,end)-step,0), step)
+
+def get_pixel_color(x, y, size):
+	if size <= 1:	# Optimization for one pixel resolution.
 		return pixels[x,y]
 	r,g,b = 0,0,0
 	for yy in range(y,y+size):
@@ -33,65 +37,95 @@ def get_col(x, y, size):
 	irr = 1 / (size*size)
 	return r*irr,g*irr,b*irr
 
-def col2weight(col):
+def color2weight(col):
+	'''Ignore grayscale colors, as that constitutes the background, text and lines.
+	   Yellow means low weight, red means higher weight and highest is dark red.'''
 	i = 1/255
 	r,g,b = col[0]*i,col[1]*i,col[2]*i
 	weight = (g-b) + (r-g)*2 + max(r-b-g,0)*(1-r)*5	# Yellow > red > dark red.
 	return weight/3
 
-# print(col2weight((136,1,3)))
-# print(col2weight((0,0,0)))
-# import sys
-# sys.exit()
-
-def create_level(zoom_level, map_center, map_scale, img_center, img_scale, patch_overlap):
-	if zoom_level > 8:
+def create_heat_patch(zoom_level, map_center, map_scale, img_center, img_scale, patch_overlap):
+	'''zoom_level is same as Google Maps' definition. We only create patches for even zoom levels.
+	   patch_overlap should be large enough that we don't show blank parts as we pan close to the
+	   border of the patch.'''
+	if zoom_level > 12:	# Don't recurse infinitely.
 		return
 	fname = '%g_%g_%g_popdata.json' % (zoom_level, map_center.lat(), map_center.lng())
-	if not exists(fname):
-		print(fname)
-		json = []
-		weight_limit = 0.35 - (zoom_level-6)/100
-		pixel_scale = int(img_height*img_scale*patch_overlap)
-		xo,yo = int(img_center.x-pixel_scale/2),int(img_center.y-pixel_scale/2)
-		pixel_res = int(24/zoom_level)
-		print('img X range:', xo, xo+pixel_scale)
-		print('img Y range:', yo, yo+pixel_scale)
-		for y in clamp_range(img_height, yo, yo+pixel_scale, pixel_res):
-			for x in clamp_range(img_width, xo, xo+pixel_scale, pixel_res):
-				col = get_col(x, y, pixel_res)
-				weight = col2weight(col)
-				if weight > weight_limit:
-					rel_crd = (crd(x,y) - img_center)
-					map_rel_crd = rel_crd * (map_scale/(img_height*img_scale))
-					p = map_adjust(map_rel_crd + map_center)
-					json += ['[%3.3f,%3.3f,%3.3f]' % (p.lat(),p.lng(),10**weight)]
-					# print('json=', json)
-					# print('p=', p.x, p.y)
-					# print('y=', y)
-					# print('x=', x)
-					# print('rel.y=', rel_crd.y)
-					# print('rel.x=', rel_crd.x)
-					# print('map_scale=', map_scale)
-					# import sys
-					# sys.exit()
-		if json:
-			print(' - writing')
-			f = open(fname, 'w')
-			print('[' + ','.join(json) + ']', file=f)
-		else:
-			print(' - skipping - no content')
-		print('   |', xo, yo, pixel_scale, img_center.x, img_center.y, img_scale)
-	m_quad_f,i_quad_f = map_scale/4,img_height*img_scale/4
-	create_level(zoom_level+2, map_center+crd(-m_quad_f,-m_quad_f), map_scale/2, img_center+crd(-i_quad_f,-i_quad_f), img_scale/2, patch_overlap)
-	create_level(zoom_level+2, map_center+crd(+m_quad_f,-m_quad_f), map_scale/2, img_center+crd(+i_quad_f,-i_quad_f), img_scale/2, patch_overlap)
-	create_level(zoom_level+2, map_center+crd(-m_quad_f,+m_quad_f), map_scale/2, img_center+crd(-i_quad_f,+i_quad_f), img_scale/2, patch_overlap)
-	create_level(zoom_level+2, map_center+crd(+m_quad_f,+m_quad_f), map_scale/2, img_center+crd(+i_quad_f,+i_quad_f), img_scale/2, patch_overlap)
+	if exists(fname):	# Don't redo patches.
+		return
+	if fname.startswith('12_34_62'):
+		print('*********************************')
+	#print(fname)
+
+	json = []
+	img_size = img_width if img_width>img_height else img_height
+	# Adjust X and Y pixels so map_adjust() won't put the points outside of the patch.
+	pixel_adjust = (map_inverse_adjust(map_center) - map_center) * (img_scale*img_size/map_scale)
+	img_adjusted_center = img_center + pixel_adjust
+	# print(img_center.to_coord_str())
+	# print(img_adjusted_center.to_coord_str())
+	# print(pixel_adjust.to_coord_str())
+	# import sys
+	# sys.exit()
+	# Reduce required weight somewhat as zoom levels increase.
+	population_weight_limit = 0.45 - (zoom_level-6)/120
+	image_patch_size = int(img_size*img_scale*patch_overlap)
+	xo,yo = int(img_adjusted_center.x-image_patch_size/2),int(img_adjusted_center.y-image_patch_size/2)
+	# Scale factor from image pixels to map (optimization for inner-most loop).
+	img2map_scale = map_scale / (img_size*img_scale)
+	# Increase pixel-to-map resolution in higher zoom.
+	pixel_resolution = int(23/zoom_level)
+	print(fname, ' |', xo, yo, image_patch_size, pixel_resolution, 'actual xy:', img_adjusted_center.x-img_size*img_scale/2, img_adjusted_center.y-img_size*img_scale/2, 'size:', img_size*img_scale)
+	img_rel_crd = crd(-1,-1) * (img_size*img_scale/2.0)
+	map_crd = img_rel_crd * img2map_scale + map_center
+	img_rel_crd = crd(+1,+1) * (img_size*img_scale/2.0)
+	map_crd_lr = img_rel_crd * img2map_scale + map_center
+	print('     upper-left map corner:', map_crd, '    lower-right map corner:', map_crd_lr)
+	for y in clamp_range(img_height, yo, yo+image_patch_size, pixel_resolution):
+		for x in clamp_range(img_width, xo, xo+image_patch_size, pixel_resolution):
+			col = get_pixel_color(x, y, pixel_resolution)
+			population_weight = color2weight(col)	# Convert pixel color to weight.
+			if population_weight > population_weight_limit:
+				img_rel_crd = crd(x,y) - img_center
+				map_crd = img_rel_crd * img2map_scale + map_center
+				# assert map_crd.lat() >= map_center.lat()-map_scale*0.6*patch_overlap, 'map:%s, center:%s, scale:%g' % (map_crd,map_center,map_scale)
+				# assert map_crd.lat() <= map_center.lat()+map_scale*0.6*patch_overlap, 'map:%s, center:%s, scale:%g' % (map_crd,map_center,map_scale)
+				# assert map_crd.lng() >= map_center.lng()-map_scale*0.6*patch_overlap, 'map:%s, center:%s, scale:%g' % (map_crd,map_center,map_scale)
+				# assert map_crd.lng() <= map_center.lng()+map_scale*0.6*patch_overlap, 'map:%s, center:%s, scale:%g' % (map_crd,map_center,map_scale)
+				p = map_adjust(map_crd)
+				if fname.startswith('12_34_62'):
+					print(p, '|', x, y, img2map_scale, '|', map_center, '|', img_center.to_coord_str(), '|', img_adjusted_center.to_coord_str())
+				json += ['[%3.3f,%3.3f,%3.3f]' % (p.lat(),p.lng(),10**population_weight)]
+	if json:
+		p = map_adjust(map_center+crd(-1,-1)*(map_scale/2))
+		q = map_adjust(map_center+crd(-1,+1)*(map_scale/2))
+		r = map_adjust(map_center+crd(+1,-1)*(map_scale/2))
+		s = map_adjust(map_center+crd(+1,+1)*(map_scale/2))
+		json += ['[%3.3f,%3.3f,%3.3f]' % (p.lat(),p.lng(),1)]
+		json += ['[%3.3f,%3.3f,%3.3f]' % (q.lat(),q.lng(),1)]
+		json += ['[%3.3f,%3.3f,%3.3f]' % (r.lat(),r.lng(),1)]
+		json += ['[%3.3f,%3.3f,%3.3f]' % (s.lat(),s.lng(),1)]
+		json = '[' + ','.join(json) + ']'
+		print('%s: %i bytes.' % (fname, len(json)))
+		open(fname, 'w').write(json)
+	# Create sub-patches for each quadrant.
+	m_quad_f,i_quad_f = map_scale/4,img_size*img_scale/4
+	create_heat_patch(zoom_level+2, map_center+crd(-m_quad_f,-m_quad_f), map_scale/2, img_center+crd(-i_quad_f,-i_quad_f), img_scale/2, patch_overlap)
+	create_heat_patch(zoom_level+2, map_center+crd(+m_quad_f,-m_quad_f), map_scale/2, img_center+crd(+i_quad_f,-i_quad_f), img_scale/2, patch_overlap)
+	create_heat_patch(zoom_level+2, map_center+crd(-m_quad_f,+m_quad_f), map_scale/2, img_center+crd(-i_quad_f,+i_quad_f), img_scale/2, patch_overlap)
+	create_heat_patch(zoom_level+2, map_center+crd(+m_quad_f,+m_quad_f), map_scale/2, img_center+crd(+i_quad_f,+i_quad_f), img_scale/2, patch_overlap)
 
 
-[remove(f) for f in glob('*.json')]
-map_center,map_scale = afghanistan,32
+[remove(f) for f in glob('*.json')]	# Drop all previously generated .json files.
+
+# Setup data, load image and create heat patches recursively.
+country_center = latlng(32,64)	# ~Afghanistan.
+img = Image.open('af_population.png')
+pixels = img.load()
+img_width,img_height = img.size
+map_center,map_scale = country_center,32
 img_center,img_scale = crd(img_width/2,img_height/2),1
 patch_overlap = 3
 zoom_level = 6
-create_level(zoom_level, map_center, map_scale, img_center, img_scale, patch_overlap)
+create_heat_patch(zoom_level, map_center, map_scale, img_center, img_scale, patch_overlap)
